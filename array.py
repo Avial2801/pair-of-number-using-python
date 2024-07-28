3c1a667edf166642f085e663d59c319cab831a10
Avial2801 <pandeyaviral01@gmail.com>
2024-07-28 17:04:42 +0530

    Add files via upload

---
+++ array.py
@@ -0,0 +1,17 @@
+l = [9,3,5,4,6,7,8,1,0]
+l.sort()
+print (l)
+target = int(input("enter the number"))
+left = 0
+right = len(l) - 1
+while (left <= right ):
+    if (l[left] + l[right] > target):
+        right = right -1
+    elif (l[left] + l[right] < target):
+        left = left + 1
+    elif (l[left] + l[right] == target):
+        print (l[left] , l[right])
+        right = right -1
+        left = left + 1
+
+        
